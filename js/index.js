b53744d2f94f0c0717f9dd00a6ff3247cb59c4af
Ender Minyard <60559370+genderev@users.noreply.github.com>
2021-03-12 04:29:57 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,15 +17,13 @@ let navigation = (linkGraph) => {
       //if there's already inbound links to this page
       let oldArray = linkGraph.get(prev); //let's get the array that stored the urls for the old inbound links
       oldArray.push(location.href) //let's create a new entry in the array for the current document url
-      let set = [...new Set(oldArray)]
-      linkGraph.set(prev, set); //let's save the modified array to localStorage under the page name
+      linkGraph.set(prev, oldArray); //let's save the modified array to localStorage under the page name
       console.log("in linkGraph.get(prev)");
     } else if (!linkGraph.get(prev)) {
       // this is if there are no previous inbound links for this page
       let newArray = []; //create a new array to store inbound links
       newArray.push(location.href); //let's create a new entry in the array for the current document url
-      let set = [...new Set(newArray)]
-      linkGraph.set(prev, set); //let's save this new array to localStorage under the page name
+      linkGraph.set(prev, newArray); //let's save this new array to localStorage under the page name
       console.log("!linkGraph.get(prev)");
     }
  }
